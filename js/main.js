44c8b5da6494667a5405a5cbd5b54a4149415149
Ian T. Nordeck <nordeck@Ians-MacBook-Pro-2.local>
2017-06-14 16:16:01 -0600

    created game logic unit 10

--- js/main.js
+++ js/main.js
@@ -1,17 +1,40 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "Queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "Queen",
+		suit: "diamonss",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "King",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "King",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function () {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-			console.log("You found a match!");
+			alert("You found a match!");
 		} else {
-			console.log("Sorry, try again."); 
+			alert("Sorry, try again."); 
 		}
 	};
 
 var flipCard = function (cardId) {
-	console.log( "Player flipped " + cards[cardId] + ".");
-	cardsInPlay.push(cards[cardId]);
+	console.log( "Player flipped " + cards[cardId].rank + ".");
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
 	};
 
