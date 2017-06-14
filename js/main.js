06a7dc6c96ba2f87590667d760f7994464cdf6e7
Ian T. Nordeck <nordeck@Ians-MacBook-Pro-2.local>
2017-06-14 14:41:48 -0600

    created game logic exercise 9

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("Player flipped queen");
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("Player flipped king");
-if (cardsInPlay.length === 2);
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again."); 
-	}
+
+var checkForMatch = function () {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again."); 
+		}
+	};
+
+var flipCard = function (cardId) {
+	console.log( "Player flipped " + cards[cardId] + ".");
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+	};
+
+flipCard(0);
+flipCard(2);
+
+//	var cardOne = cards[0];
+//	cardsInPlay.push(cardOne);
+//	console.log("Player flipped queen");
+//	var cardTwo = cards[3];
+//	cardsInPlay.push(cardTwo);
+//	console.log("Player flipped king");
