9230214d2a39afa9ae461bf805cdbc2c098bc297
Ian T. Nordeck <nordeck@Ians-MacBook-Pro-2.local>
2017-06-15 18:21:03 -0600

    created broken game logic for exercise 11

--- js/main.js
+++ js/main.js
@@ -1,49 +1,52 @@
 var cards = [
-	{
-		rank: "Queen",
-		suit: "hearts",
-		cardImage: "images/queen-of-hearts.png"
-	},
-	{
-		rank: "Queen",
-		suit: "diamonss",
-		cardImage: "images/queen-of-diamonds.png"
-	},
-	{
-		rank: "King",
-		suit: "hearts",
-		cardImage: "images/king-of-hearts.png"
-	},
-	{
-		rank: "King",
-		suit: "diamonds",
-		cardImage: "images/king-of-diamonds.png"
-	}
+ {
+  rank: "Queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+ },
+ {
+  rank: "Queen",
+  suit: "diamonss",
+  cardImage: "images/queen-of-diamonds.png"
+ },
+ {
+   rank: "King",
+   suit: "hearts",
+   cardImage: "images/king-of-hearts.png"
+ },
+ {
+   rank: "King",
+   suit: "diamonds",
+   cardImage: "images/king-of-diamonds.png"
+ }
 ];
 var cardsInPlay = [];
 
 var checkForMatch = function () {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
-		} else {
-			alert("Sorry, try again."); 
-		}
-	};
+ this.setAttribute(cards[cardId].cardImage);
+ if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+ } else {
+  alert("Sorry, try again.");
+ }
+};
 
-var flipCard = function (cardId) {
-	console.log( "Player flipped " + cards[cardId].rank + ".");
-	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-	checkForMatch();
-	};
+var flipCard = function () {
+ var cardID = this.getAttribute("data-id");
+ console.log( "Player flipped " + cards[cardId].rank + ".");
+ cardsInPlay.push(cards[cardId].rank);
+ console.log(cards[cardId].cardImage);
+ console.log(cards[cardId].suit);
+ checkForMatch();
+};
 
-flipCard(0);
-flipCard(2);
-
-//	var cardOne = cards[0];
-//	cardsInPlay.push(cardOne);
-//	console.log("Player flipped queen");
-//	var cardTwo = cards[3];
-//	cardsInPlay.push(cardTwo);
-//	console.log("Player flipped king");
+var createBoard = function () {
+ for (i = 0; i < cards.length; i++) {
+ var cardElement = document.createElement("img");
+ cardElement.setAttribute("cardElement", src = "images/back.png");
+ cardElement.setAttribute("data-id", i);
+ cardElement.addEventListener("click", flipCard);
+ document.getElementById("game-board").appendChild(cardElement);
+ }
+};
+createBoard();
